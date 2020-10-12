e274dec9503ccc925d52304934e42d307e84eeb4
David Cooper <dmcooper2412@gmail.com>
2020-10-12 19:27:08 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+
+const httpServer = require('http').createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', Buffer.byteLength(content));
+    res.end(content);
+});
+
+const io = require('socket.io')(httpServer);
+
+io.on('connect', socket => {
+
+    socket.emit('serverMessage', 'message from server connection to socket');
+    
+    socket.on('chat message', message => {
+        console.log(message);
+        io.emit('server message', message);
+    });
+
+    socket.on('user connected', user => {
+        console.log(`${user} signed on.`)
+    });
+
+    socket.on('user disconnected', user => {
+        console.log(`${user} signed off.`)
+    });
+});
+
+httpServer.listen(3000, () => {
+    console.log('go to http://localhost:3000');
+});
